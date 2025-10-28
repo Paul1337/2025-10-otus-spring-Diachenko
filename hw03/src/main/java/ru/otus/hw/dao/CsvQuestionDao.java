67c8b5eba737aae5de2e6acd9fd2e8c804a06bdd
Paul <p@x128.ru>
2025-10-28 14:25:45 +0300

    hw03

---
+++ hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,45 @@
+package ru.otus.hw.dao;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class CsvQuestionDao implements QuestionDao {
+    private final TestFileNameProvider fileNameProvider;
+
+    private InputStream getResourceAsStream(String fileName) {
+        InputStream stream = getClass().getClassLoader().getResourceAsStream(fileName);
+        if (stream == null) {
+            throw new QuestionReadException("Required resource '%s' not found in classpath. ".formatted(fileName));
+        }
+        return stream;
+    }
+
+    @Override
+    public List<Question> findAll() {
+        String fileName = fileNameProvider.getTestFileName();
+        try (InputStream testFileInputStream = getResourceAsStream(fileName);
+             InputStreamReader inputStreamReader = new InputStreamReader(testFileInputStream);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        ) {
+            var csvToQuestionDtoBuilder = new CsvToBeanBuilder<QuestionDto>(bufferedReader);
+            var questionDtos = csvToQuestionDtoBuilder.withType(QuestionDto.class)
+                    .withSkipLines(1).withSeparator(';').build().parse();
+            return questionDtos.stream().map(QuestionDto::toDomainObject).toList();
+        } catch (IOException e) {
+            throw new QuestionReadException("File with test questions not found", e);
+        }
+    }
+}
