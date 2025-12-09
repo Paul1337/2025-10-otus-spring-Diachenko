54aabe1adee29f62bbd8c232d1961718dfafb1e7
Paul <p@x128.ru>
2025-12-09 12:04:54 +0300

    hw07

---
+++ hw07/src/main/java/ru/otus/hw/hw06/services/BookServiceImpl.java
@@ -0,0 +1,95 @@
+package ru.otus.hw.hw06.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.hw06.dto.BookDto;
+import ru.otus.hw.hw06.exceptions.EntityNotFoundException;
+import ru.otus.hw.hw06.mappers.BookMapper;
+import ru.otus.hw.hw06.models.Author;
+import ru.otus.hw.hw06.models.Book;
+import ru.otus.hw.hw06.models.Genre;
+import ru.otus.hw.hw06.repositories.AuthorRepository;
+import ru.otus.hw.hw06.repositories.BookRepository;
+import ru.otus.hw.hw06.repositories.GenreRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.springframework.util.CollectionUtils.isEmpty;
+
+@RequiredArgsConstructor
+@Service
+public class BookServiceImpl implements BookService {
+    private final AuthorRepository authorRepository;
+
+    private final GenreRepository genreRepository;
+
+    private final BookRepository bookRepository;
+
+    private final BookMapper bookMapper;
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<BookDto> findById(long id) {
+        var book = bookRepository.findById(id);
+        return book.map(bookMapper::bookToDto);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<BookDto> findAll() {
+        var books = bookRepository.findAll();
+        return books.stream().map(bookMapper::bookToDto).collect(Collectors.toList());
+    }
+
+    @Transactional
+    @Override
+    public BookDto insert(String title, long authorId, Set<Long> genresIds) {
+        var author = findAuthorById(authorId);
+        var genres = findGenresByIds(genresIds);
+        var newBook = new Book(0, title, author, genres);
+        var savedBook = bookRepository.save(newBook);
+        return bookMapper.bookToDto(savedBook);
+    }
+
+    @Transactional
+    @Override
+    public BookDto update(long id, String title, long authorId, Set<Long> genresIds) {
+        var book = findBookById(id);
+        var author = findAuthorById(authorId);
+        var genres = findGenresByIds(genresIds);
+
+        book.setTitle(title);
+        book.setAuthor(author);
+        book.setGenres(genres);
+
+        var savedBook = bookRepository.save(book);
+        return bookMapper.bookToDto(savedBook);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(long id) {
+        bookRepository.deleteById(id);
+    }
+
+    private Author findAuthorById(long id) {
+        return authorRepository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException("Author with id %d not found".formatted(id)));
+    }
+
+    private Book findBookById(long id) {
+        return bookRepository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException("Book with id %d not found".formatted(id)));
+    }
+
+    private List<Genre> findGenresByIds(Set<Long> ids) {
+        var genres = genreRepository.findAllById(ids);
+        if (isEmpty(genres) || ids.size() != genres.size()) {
+            throw new EntityNotFoundException("One or all genres with ids %s not found".formatted(ids));
+        }
+        return genres;
+    }
+}
---
+++ hw07/src/test/java/ru/otus/hw/hw06/repositories/JpaBookRepositoryTest.java
@@ -0,0 +1,125 @@
+package ru.otus.hw.hw06.repositories;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import ru.otus.hw.hw06.models.Author;
+import ru.otus.hw.hw06.models.Book;
+import ru.otus.hw.hw06.models.Genre;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Репозиторий на основе Jpa для работы с книгами")
+@DataJpaTest
+public class JpaBookRepositoryTest {
+    @Autowired
+    private BookRepository repository;
+
+    @Autowired
+    private TestEntityManager em;
+
+    private static final int BOOKS_COUNT = 3;
+
+    @DisplayName("должен загружать книгу по id")
+    @ParameterizedTest
+    @ValueSource(longs = {1, 2, 3})
+    void shouldReturnCorrectBookById(long bookId) {
+        var actualBook = repository.findById(bookId);
+        assertThat(actualBook).isPresent();
+
+        em.clear();
+        var expectedBook = em.find(Book.class, bookId);
+
+        assertThat(actualBook).isPresent()
+                .get()
+                .usingRecursiveComparison()
+//                .ignoringFields("genres")
+                .isEqualTo(expectedBook);
+    }
+
+    @DisplayName("должен загружать список всех книг")
+    @Test
+    void shouldReturnCorrectBooksList() {
+        var actualBooks = repository.findAll();
+        assertThat(actualBooks).hasSize(BOOKS_COUNT);
+        assertThat(actualBooks)
+                .allMatch(book -> book.getId() > 0)
+                .allMatch(book -> book.getAuthor() != null)
+                .allMatch(book -> book.getTitle() != null)
+                .allMatch(book -> book.getGenres() != null && !book.getGenres().isEmpty());
+    }
+
+    @DisplayName("должен сохранять новую книгу")
+    @Test
+    void shouldSaveNewBook() {
+        var author = em.find(Author.class, 1);
+        var genres = List.of(
+                em.find(Genre.class, 1),
+                em.find(Genre.class, 2)
+        );
+
+        var expectedBook = new Book(0, "BookTitle_10500", author, genres);
+        var returnedBook = repository.save(expectedBook);
+
+        assertThat(returnedBook).isNotNull()
+                .matches(book -> book.getId() > 0)
+                .usingRecursiveComparison().ignoringExpectedNullFields().isEqualTo(expectedBook);
+
+        em.flush();
+        em.clear();
+
+        assertThat(repository.findById(returnedBook.getId()))
+                .isPresent()
+                .get()
+                .usingRecursiveComparison()
+                .ignoringExpectedNullFields()
+                .isEqualTo(returnedBook);
+    }
+
+    @DisplayName("должен сохранять измененную книгу")
+    @Test
+    void shouldSaveUpdatedBook() {
+        var author = em.find(Author.class, 2);
+        var genres = List.of(
+                em.find(Genre.class, 4),
+                em.find(Genre.class, 5)
+        );
+        var expectedBook = new Book(1L, "BookTitle_10500", author, genres);
+
+        assertThat(repository.findById(expectedBook.getId()))
+                .isPresent()
+                .get()
+                .isNotEqualTo(expectedBook);
+
+        var returnedBook = repository.save(expectedBook);
+        assertThat(returnedBook).isNotNull()
+                .matches(book -> book.getId() > 0)
+                .usingRecursiveComparison()
+                .ignoringExpectedNullFields()
+                .isEqualTo(expectedBook);
+
+        em.flush();
+        em.clear();
+
+        assertThat(repository.findById(returnedBook.getId()))
+                .isPresent()
+                .get()
+                .usingRecursiveComparison()
+                .ignoringExpectedNullFields()
+                .isEqualTo(returnedBook);
+    }
+
+    @DisplayName("должен удалять книгу по id")
+    @ParameterizedTest
+    @ValueSource(longs = { 1, 2, 3 })
+    void shouldDeleteBook(long bookId) {
+        assertThat(repository.findById(bookId)).isPresent();
+        repository.deleteById(bookId);
+        assertThat(repository.findById(bookId)).isEmpty();
+    }
+
+}
