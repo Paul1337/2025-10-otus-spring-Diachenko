5106703c44639c5d2c13318e9c6cec2df28f7fd2
Paul <p@x128.ru>
2026-02-21 17:49:49 +0300

    fix/checkstyle

--- hw10/src/main/java/ru/otus/hw/hw10/dto/CreateCommentDto.java
+++ hw10/src/main/java/ru/otus/hw/hw10/dto/CreateCommentDto.java
@@ -1,11 +1,9 @@
 package ru.otus.hw.hw10.dto;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 
 @Data
 @NoArgsConstructor
